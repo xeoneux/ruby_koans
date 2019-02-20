10878a99b26754e5d783ec6dc4995d771f50ac18
Aayush Kapoor <xeoneux@icloud.com>
2019-02-20 15:45:42 +0530

    Conclude constants

--- about_constants.rb
+++ about_constants.rb
@@ -1,32 +1,37 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-C = "top level"
+C = 'top level'
 
+# AboutConstants
 class AboutConstants < Neo::Koan
-
-  C = "nested"
+  C = 'nested'
 
   def test_nested_constants_may_also_be_referenced_with_relative_paths
-    assert_equal __, C
+    assert_equal 'nested', C
   end
 
   def test_top_level_constants_are_referenced_by_double_colons
-    assert_equal __, ::C
+    assert_equal 'top level', ::C
   end
 
   def test_nested_constants_are_referenced_by_their_complete_path
-    assert_equal __, AboutConstants::C
-    assert_equal __, ::AboutConstants::C
+    assert_equal 'nested', AboutConstants::C
+    assert_equal 'nested', ::AboutConstants::C
   end
 
   # ------------------------------------------------------------------
 
+  # Animal
   class Animal
     LEGS = 4
     def legs_in_animal
       LEGS
     end
 
+    # NestedAnimal
     class NestedAnimal
       def legs_in_nested_animal
         LEGS
@@ -35,11 +40,12 @@ class AboutConstants < Neo::Koan
   end
 
   def test_nested_classes_inherit_constants_from_enclosing_classes
-    assert_equal __, Animal::NestedAnimal.new.legs_in_nested_animal
+    assert_equal 4, Animal::NestedAnimal.new.legs_in_nested_animal
   end
 
   # ------------------------------------------------------------------
 
+  # Reptile
   class Reptile < Animal
     def legs_in_reptile
       LEGS
@@ -47,14 +53,16 @@ class AboutConstants < Neo::Koan
   end
 
   def test_subclasses_inherit_constants_from_parent_classes
-    assert_equal __, Reptile.new.legs_in_reptile
+    assert_equal 4, Reptile.new.legs_in_reptile
   end
 
   # ------------------------------------------------------------------
 
+  # MyAnimals
   class MyAnimals
     LEGS = 2
 
+    # Bird
     class Bird < Animal
       def legs_in_bird
         LEGS
@@ -63,7 +71,7 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_both_nested_and_inherited_constants
-    assert_equal __, MyAnimals::Bird.new.legs_in_bird
+    assert_equal 2, MyAnimals::Bird.new.legs_in_bird
   end
 
   # QUESTION: Which has precedence: The constant in the lexical scope,
@@ -71,6 +79,7 @@ class AboutConstants < Neo::Koan
 
   # ------------------------------------------------------------------
 
+  # Oyster
   class MyAnimals::Oyster < Animal
     def legs_in_oyster
       LEGS
@@ -78,7 +87,7 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_explicit_scoping_on_class_definition
-    assert_equal __, MyAnimals::Oyster.new.legs_in_oyster
+    assert_equal 4, MyAnimals::Oyster.new.legs_in_oyster
   end
 
   # QUESTION: Now which has precedence: The constant in the lexical
