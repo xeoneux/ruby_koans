c84db2abff6b7a3f95dc6d81c948434c8e9deeee
Aayush Kapoor <xeoneux@icloud.com>
2019-02-21 15:07:18 +0530

    Defeat dice project

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -1,11 +1,19 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+# DiceSet
+class DiceSet
+  attr_reader :values
 
+  def roll(num)
+    @values = (1..num).map { rand(1..6) }
+  end
+end
+
+# AboutDiceProject
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
@@ -16,7 +24,7 @@ class AboutDiceProject < Neo::Koan
     dice = DiceSet.new
 
     dice.roll(5)
-    assert dice.values.is_a?(Array), "should be an array"
+    assert dice.values.is_a?(Array), 'should be an array'
     assert_equal 5, dice.values.size
     dice.values.each do |value|
       assert value >= 1 && value <= 6, "value #{value} must be between 1 and 6"
@@ -41,7 +49,7 @@ class AboutDiceProject < Neo::Koan
     second_time = dice.values
 
     assert_not_equal first_time, second_time,
-      "Two rolls should not be equal"
+                     'Two rolls should not be equal'
 
     # THINK ABOUT IT:
     #
@@ -59,5 +67,4 @@ class AboutDiceProject < Neo::Koan
     dice.roll(1)
     assert_equal 1, dice.values.size
   end
-
 end
